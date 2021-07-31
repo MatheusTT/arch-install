ad9614bd1f74ecfc28f7b1ca4b0c7ffd9153bb58
Matheus <broa@arch-linux.localdomain>
2021-07-31 12:19:34 -0300

    modified some things and the echo .zshrc is now on adding-to-zshrc.sh

--- post-installation.sh
+++ post-installation.sh
@@ -14,9 +14,9 @@ pikaur -S --noconfirm matcha-gtk-theme
 
 sudo pacman -S --noconfirm papirus-icon-theme
 
-sudo cz-Hickson-Black /usr/share/icons/
+sudo mv cz-Hickson-Black /usr/share/icons/
 
-## Powerlevel10k and zsh (and pfetch)
+## Powerlevel10k and and pfetch
 pikaur -S --noconfirm pfetch
 
 pikaur -S --noconfirm nerd-fonts-meslo
@@ -24,26 +24,9 @@ pikaur -S --noconfirm nerd-fonts-meslo
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.local/share/powerlevel10k
 echo 'source ~/.local/share/powerlevel10k/powerlevel10k.zsh-theme' >> $HOME/.zshrc
 
-sudo pacman -S --noconfirm zsh-syntax-highlighting zsh-autosuggestions
-
-echo "
-## For saving commands 
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
-
-## Plugins
-# zsh-syntax-highlighting
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-# zsh-autosuggestions
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-## My own commands
-ZLE_RPROMPT_INDENT=0
-pfetch" >> $HOME/.zshrc
 
 ## Kitty conf
 # first the fonts
 sudo pacman -S --noconfirm ttf-fira-{code,mono,sans}
 
-cp ./config_files/kitty.conf $HOME/.config/kitty/
+mv ./config_files/kitty.conf $HOME/.config/kitty/kitty.conf
