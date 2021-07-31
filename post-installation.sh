8169df145eba6f1b562b885b4e71b4b49fcd23cd
Matheus <broa@arch-linux.localdomain>
2021-07-31 11:25:22 -0300

    Tweaked some commands

--- post-installation.sh
+++ post-installation.sh
@@ -14,7 +14,7 @@ pikaur -S --noconfirm matcha-gtk-theme
 
 sudo pacman -S --noconfirm papirus-icon-theme
 
-cd $HOME/Downloads && wget -nv https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MjU1ODA0OTIiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjU0MDg3NDllMTIyYjFkMGZjZGZhZWQ5OGIyMmY5ZDY4N2EyNDlkZDQxMGMwNmVjZGUxNTU4NzVlOWYwMTE3MmJiMGQwZWQ3MGE5YWEwZTljZGYxODk4YTVmYzliNmY0ZTM0YzZiNGRkYTRiNjJjYTNjNWJjN2Q0MjQ4NTI2Yzg5IiwidCI6MTYyNjk3NjI4OSwic3RmcCI6ImJlYzhmMGRmYTUyYzRjZWM1ZDY3NDgwNzMwNDM5NzdjIiwic3RpcCI6IjI4MDQ6ZDU1OjUyOTM6YzYwMDoxMDNkOmFiOGE6YTFmYjoyYjVkIn0.Fw-WWWDPGIr5gVX3fcPgjWcRguC6kCVvRWuvrKDzQXg/cz-Hickson-Black.zip
+wget -nv https://dllb2.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MjU1ODA0OTIiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjU0MDg3NDllMTIyYjFkMGZjZGZhZWQ5OGIyMmY5ZDY4N2EyNDlkZDQxMGMwNmVjZGUxNTU4NzVlOWYwMTE3MmJiMGQwZWQ3MGE5YWEwZTljZGYxODk4YTVmYzliNmY0ZTM0YzZiNGRkYTRiNjJjYTNjNWJjN2Q0MjQ4NTI2Yzg5IiwidCI6MTYyNjk3NjI4OSwic3RmcCI6ImJlYzhmMGRmYTUyYzRjZWM1ZDY3NDgwNzMwNDM5NzdjIiwic3RpcCI6IjI4MDQ6ZDU1OjUyOTM6YzYwMDoxMDNkOmFiOGE6YTFmYjoyYjVkIn0.Fw-WWWDPGIr5gVX3fcPgjWcRguC6kCVvRWuvrKDzQXg/cz-Hickson-Black.zip
 
 unzip cz-Hickson-Black.zip
 mv cz-Hickson-Black /usr/share/icons/
@@ -24,8 +24,8 @@ pikaur -S --noconfirm pfetch
 
 pikaur -S --noconfirm nerd-fonts-meslo
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.local/share/powerlevel10k
-echo 'source ~/.local/share/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.local/share/powerlevel10k
+echo 'source ~/.local/share/powerlevel10k/powerlevel10k.zsh-theme' >> $HOME/.zshrc
 
 sudo pacman -S --noconfirm zsh-syntax-highlighting zsh-autosuggestions
 
@@ -43,7 +43,7 @@ source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 ## My own commands
 ZLE_RPROMPT_INDENT=0
-pfetch" >> .zshrc
+pfetch" >> $HOME/.zshrc
 
 ## Kitty conf
 # first the fonts
