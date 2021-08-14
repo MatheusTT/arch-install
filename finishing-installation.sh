14ce69f139d3c28d2beb4842a393296f1da71415
Matheus Teixeira Tomaz <matheusttomaz75@gmail.com>
2021-08-13 21:39:39 -0300

    modified grub, removed some packages and added some echoes

--- finishing-installation.sh
+++ finishing-installation.sh
@@ -4,7 +4,7 @@ ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc --utc
 
 LANG="pt_BR.UTF-8 UTF-8"
-sed -i "s/#$LANG/ $LANG/" /etc/locale.gen
+sed -i "s/#$LANG/$LANG/" /etc/locale.gen
 locale-gen
 
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf
@@ -26,25 +26,27 @@ passwd broa
 sed -i "82s/./ /" /etc/sudoers
 
 
-pacman -Sy grub efibootmgr dosfstools os-prober mtools networkmanager bluez tlp reflector 
+echo -e "\n\033[1;32mInstalling grub, networkmanager, reflector, etc.\033[0m"
+pacman -Sy grub efibootmgr dosfstools os-prober mtools networkmanager reflector tlp
+
+echo -e "\n\033[1;32mInstalling pipewire, wayland and xwayland\033[0m"
 pacman -S pipewire pipewire-{alsa,jack,media-session,pulse} wayland xorg-xwayland 
 
-#pacman -S nvidia nvidia-{utils,settings,prime} mesa xf86-video-intel xf86-input-libinput
+#pacman -S nvidia nvidia-{utils,settings,prime} xf86-video-intel xf86-input-libinput
 
 sed -i "52s/ keyboard//g" /etc/mkinitcpio.conf
 sed -i "52s/ autodetect/ autodetect keyboard keymap/" /etc/mkinitcpio.conf
-sed -i "52s/ block/ block encrypt/" /etc/mkinitcpio.conf
-mkinitcpio -P
+#sed -i "52s/ block/ block encrypt/" /etc/mkinitcpio.conf
+mkinitcpio -p linux
 
 
 echo -e "\n\033[1;32mgrub-install\033[0m"
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=Arch --recheck
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --recheck
 echo -e "\n\033[1;32mgrub-mkconfig\033[0m"
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\033[1;32menabling some services\033[0m"
 systemctl enable NetworkManager
-systemctl enable bluetooth
 systemctl enable tlp
 systemctl enable reflector.timer
 systemctl enable fstrim.timer
@@ -53,5 +55,4 @@ echo "
 Now just do:
 1. exit
 2. umount -R /mnt
-3. shutdown or reboot
-"
+3. shutdown or reboot"
