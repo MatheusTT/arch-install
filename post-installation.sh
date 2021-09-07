2cf283b54bc6e93fc1e2030fcb689a0f6ae6bd31
Matheus Teixeira Tomaz <matheusttomaz75@gmail.com>
2021-09-06 22:37:34 -0300

    Added some commands for gsettings and changed the pacman.conf file into a few lines of sed

--- post-installation.sh
+++ post-installation.sh
@@ -9,7 +9,7 @@ cd /tmp && git clone --depth=1 https://aur.archlinux.org/paru-bin.git
 cd paru-bin && makepkg -sic --noconfirm
 
 
-## Themes, icons and the cursor
+## Themes, icons, the cursor theme and general gsettings configurations
 paru -S --noconfirm matcha-gtk-theme
 gsettings set org.gnome.desktop.interface gtk-theme "Matcha-dark-azul"
 
@@ -20,28 +20,58 @@ sudo mv $SCRIPT_DIR/cz-Hickson-Black /usr/share/icons/
 gsettings set org.gnome.desktop.interface cursor-theme "cz-Hickson-Black"
 
 
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
+
+# fonts
+
+for font in ttf-fira-{code,mono,sans}; do
+	if ! pacman -Qs | grep -q $font; then
+		sudo pacman -S --noconfirm "$font"
+	fi
+done
+
+gsettings set org.gnome.desktop.interface font-name           'Fira Sans 12'
+gsettings set org.gnome.desktop.interface document-font-name  'Fira Sans 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code Medium 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font  'Fira Sans Bold 12'
+
+# night-light
+
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled       true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature   3150
+
+# night-light for gdm
+
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled       true
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   0
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature   3150
+
 ## Powerlevel10k and and pfetch
 paru -S --noconfirm pfetch
 
 paru -S --noconfirm nerd-fonts-meslo &&
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$USER/.local/share/powerlevel10k &&
-echo 'source ~/.local/share/powerlevel10k/powerlevel10k.zsh-theme' >> /home/$USER/.zshrc
+echo "source ~/.local/share/powerlevel10k/powerlevel10k.zsh-theme" >> /home/$USER/.zshrc
 
 
 ## Kitty conf
 # first the fonts
-if ! pacman -Qs | grep -q ttf-fira; then
-	sudo pacman -S --noconfirm ttf-fira-{code,mono,sans}
+if ! pacman -Qs | grep -q ttf-fira-code; then
+	sudo pacman -S --noconfirm ttf-fira-code
 fi
-mv $SCRIPT_DIR/config_files/kitty.conf /home/$USER/.config/kitty/kitty.conf
+mv $SCRIPT_DIR/config_files/my-kitty.conf /home/$USER/.config/kitty/
 
 ## .vimrc
 mv $SCRIPT_DIR/config_files/.vimrc /home/$USER
 
 ## pacman.conf
-sudo mv $SCRIPT_DIR/config_files/pacman.conf /etc/pacman.conf &&
-sudo pacman -Sy
+sed -i "33s/.//"        /etc/pacman.conf
+sed -i "38i\ILoveCandy" /etc/pacman.conf
+sed -i "94,95s/.//"     /etc/pacman.conf
 
 echo "
 
