4f8709c66c98c17f9412663c127e58e241c59e9f
Matheus Teixeira Tomaz <49999922+MatheusTT@users.noreply.github.com>
2021-10-01 09:48:06 -0300

    Added a sh just for gnome configurations

---
+++ gnome-configuration.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme     "Matcha-dark-azul"
+gsettings set org.gnome.desktop.interface icon-theme    "Papirus"
+gsettings set org.gnome.desktop.interface cursor-theme  "cz-Hickson-Black"
+
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface icon-theme    "Papirus"
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme  "cz-Hickson-Black"
+
+## Mouse/touchpad settings
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile   "flat"
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click "true"
+
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.mouse accel-profile   "flat"
+sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click "true"
+
+## Keyboard layout
+gsettings set org.gnome.desktop.input-sources sources "[('xkb','br')]"
+
+## Fonts
+gsettings set org.gnome.desktop.interface font-name           'Fira Sans 12'
+gsettings set org.gnome.desktop.interface document-font-name  'Fira Sans 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code Medium 11'
+gsettings set org.gnome.desktop.wm.preferences titlebar-font  'Fira Sans Bold 12'
+
+## Night-light
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled       true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature   3150
+
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled       true
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   0
+sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature   3150
+
+## Other gsettings
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 1800
+
+gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
+gsettings set org.gnome.desktop.interface enable-hot-corners "true"
+gsettings set org.gtk.Settings.FileChooser sort-directories-first "true"
--- post-installation.sh
+++ post-installation.sh
@@ -14,23 +14,6 @@ paru -S --noconfirm matcha-gtk-theme papirus-icon-theme
 
 sudo mv $SCRIPT_DIR/cz-Hickson-Black /usr/share/icons/
 
-gsettings set org.gnome.desktop.interface gtk-theme     "Matcha-dark-azul"
-gsettings set org.gnome.desktop.interface icon-theme    "Papirus"
-gsettings set org.gnome.desktop.interface cursor-theme  "cz-Hickson-Black"
-
-sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface icon-theme    "Papirus"
-sudo -u gdm dbus-launch gsettings set org.gnome.desktop.interface cursor-theme  "cz-Hickson-Black"
-
-## Mouse/touchpad settings
-gsettings set org.gnome.desktop.peripherals.mouse accel-profile   "flat"
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click  true
-
-sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.mouse accel-profile   "flat"
-sudo -u gdm dbus-launch gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click  true
-
-## Keyboard layout
-gsettings set org.gnome.desktop.input-sources sources "[('xkb','br')]"
-
 ## Fonts
 for font in ttf-fira-{code,mono,sans}; do
 	if ! pacman -Qs | grep -q $font; then
@@ -38,29 +21,8 @@ for font in ttf-fira-{code,mono,sans}; do
 	fi
 done
 
-gsettings set org.gnome.desktop.interface font-name           'Fira Sans 12'
-gsettings set org.gnome.desktop.interface document-font-name  'Fira Sans 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code Medium 11'
-gsettings set org.gnome.desktop.wm.preferences titlebar-font  'Fira Sans Bold 12'
-
-## Night-light
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled       true
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
-gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   0
-gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature   3150
-
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled       true
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   0
-sudo -u gdm dbus-launch gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature   3150
-
-## Other gsettings
-gsettings set org.gnome.desktop.session idle-delay 0
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 1800
-
-gsettings set org.gnome.desktop.wm.preferences button-layout "appmenu:minimize,maximize,close"
-gsettings set org.gnome.desktop.interface enable-hot-corners true
-gsettings set org.gtk.Settings.FileChooser sort-directories-first true
+## Gnome configuration
+sh $SCRIPT_DIR/gnome-configuration.sh
 
 ## Powerlevel10k and and pfetch
 paru -S --noconfirm pfetch
