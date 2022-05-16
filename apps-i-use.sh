f7275bf8666a664c79f8b65d9d5508f32ab85dae
Matheus Teixeira Tomaz <49999922+MatheusTT@users.noreply.github.com>
2022-05-16 10:47:19 -0300

    Identation and formatting.

--- apps-i-use.sh
+++ apps-i-use.sh
@@ -1,54 +1,54 @@
 #!/bin/bash
 
 if [[ "$(whoami)" == "root" ]]; then
-    echo "Script must be run as a normal user" && exit
+  echo "Script must be run as a normal user" && exit
 fi
 
 PACKAGES=(
-    file-roller
-    eog
-    vlc
-    blueberry
-    gnome-calculator
-    flatpak
-    discord
-    steam-native-runtime
-    qbittorrent
-    telegram-desktop
-    btop
-    ranger
-    python-pillow
-    neofetch
-    rofi
-    gcolor3
-    ffmpegthumbnailer
-    imagemagick
-    virtualbox-host-modules-arch
-    virtualbox
-    gdu
-    yt-dlp
+  file-roller
+  eog
+  vlc
+  blueberry
+  gnome-calculator
+  flatpak
+  discord
+  steam-native-runtime
+  qbittorrent
+  telegram-desktop
+  btop
+  ranger
+  python-pillow
+  neofetch
+  rofi
+  gcolor3
+  ffmpegthumbnailer
+  imagemagick
+  virtualbox-host-modules-arch
+  virtualbox
+  gdu
+  yt-dlp
 )
 
 AUR_PACKAGES=(
-    google-chrome
-    pfetch
-    rxfetch
-    timeshift
-    visual-studio-code-bin
-    ipman
-    lib32-mangohud-x11
-    mangohud-common-x11
-    spotify
-    nerd-fonts-fira-code
-    nerd-fonts-jetbrains-mono
-    ttf-twemoji
-    ttf-ms-fonts
-    optimus-manager
-    mugshot
+  google-chrome
+  pfetch
+  rxfetch
+  timeshift
+  visual-studio-code-bin
+  ipman
+  lib32-mangohud-x11
+  mangohud-common-x11
+  spotify
+  nerd-fonts-fira-code
+  nerd-fonts-jetbrains-mono
+  ttf-twemoji
+  ttf-ms-fonts
+  optimus-manager
+  mugshot
 )
 FLATPAKS=(
-    org.gimp.GIMP
-    org.kde.kdenlive
+  org.gimp.GIMP
+  org.kde.kdenlive
 )
 
 
@@ -56,14 +56,14 @@ FLATPAKS=(
 ## Arch
 echo -e "\033[1;34mArch Packages:\033[0m"
 
-for package in ${PACKAGES[@]} ; do
-    if ( ! pacman -Qs "^$package" >/dev/null ) ; then
-        echo -e "\033[0;32m[$package]\033[0m - Installing..."
-        sudo pacman -S --noconfirm $package >/dev/null &&
-        echo -e "\033[0;32m[$package]\033[0m - Installed successfully!\n"
-    else
-        echo -e "\033[0;32m[$package]\033[0m - Already installed"
-    fi
+for package in ${PACKAGES[@]}; do
+  if ( ! pacman -Qs "^$package" >/dev/null ); then
+    echo -e "\033[0;32m[$package]\033[0m - Installing..."
+    sudo pacman -S --noconfirm $package >/dev/null &&
+    echo -e "\033[0;32m[$package]\033[0m - Installed successfully!\n"
+  else
+    echo -e "\033[0;32m[$package]\033[0m - Already installed"
+  fi
 done
 
 
@@ -71,20 +71,20 @@ done
 ## AUR
 echo -e "\n\033[1;34mAUR Packages:\033[0m"
 
-if ( ! pacman -Qs "^paru" >/dev/null ) ; then
-    cd /tmp && git clone --depth=1 https://aur.archlinux.org/paru.git 1>/dev/null
-    cd paru && makepkg -sic --noconfirm 1>/dev/null 
+if ( ! pacman -Qs "^paru" >/dev/null ); then
+  cd /tmp && git clone --depth=1 https://aur.archlinux.org/paru.git 1>/dev/null
+  cd paru && makepkg -sic --noconfirm 1>/dev/null 
 fi
 
 
-for aur_pkg in ${AUR_PACKAGES[@]} ; do
-    if ( ! pacman -Qs "^$aur_pkg" >/dev/null ) ; then
-        echo -e "\033[0;32m[$aur_pkg]\033[0m - Installing..."
-        paru -S --noconfirm "$aur_pkg" &&
-        echo -e "\033[0;32m[$aur_pkg]\033[0m - Installed successfully!\n"
-    else
-        echo -e "\033[0;32m[$aur_pkg]\033[0m - Already installed"
-    fi
+for aur_pkg in ${AUR_PACKAGES[@]}; do
+  if ( ! pacman -Qs "^$aur_pkg" >/dev/null ); then
+    echo -e "\033[0;32m[$aur_pkg]\033[0m - Installing..."
+    paru -S --noconfirm "$aur_pkg" &&
+    echo -e "\033[0;32m[$aur_pkg]\033[0m - Installed successfully!\n"
+  else
+    echo -e "\033[0;32m[$aur_pkg]\033[0m - Already installed"
+  fi
 done
 
 
@@ -93,14 +93,14 @@ done
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 echo -e "\n\033[1;36mFlatpak Packages:\033[0m"
 
-for flatpak_pkg in ${FLATPAKS[@]} ; do
-    if ( ! flatpak list | grep -q $flatpak_pkg ) ; then
-        echo -e "\033[0;32m[$flatpak_pkg]\033[0m - Installing..."
-        flatpak install -y "$flatpak_pkg" &&
-        echo -e "\033[0;32m[$flatpak_pkg]\033[0m - Installed successfully!\n"
-    else
-        echo -e "\033[0;32m[$flatpak_pkg]\033[0m - Already installed"
-    fi
+for flatpak_pkg in ${FLATPAKS[@]}; do
+  if ( ! flatpak list | grep -q $flatpak_pkg ); then
+    echo -e "\033[0;32m[$flatpak_pkg]\033[0m - Installing..."
+    flatpak install -y "$flatpak_pkg" &&
+    echo -e "\033[0;32m[$flatpak_pkg]\033[0m - Installed successfully!\n"
+  else
+    echo -e "\033[0;32m[$flatpak_pkg]\033[0m - Already installed"
+  fi
 done
 
 ## Must enable cronie.service for timeshift to work
@@ -109,33 +109,33 @@ sudo systemctl enable cronie.service
 
 ## PhotoGIMP
 if ( flatpak list | grep -q org.gimp.GIMP ) && [[ ! -f ~/.local/share/applications/org.gimp.GIMP.desktop ]]; then
-    cd ~/Downloads && wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
-    unzip PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
+  cd ~/Downloads && wget https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
+  unzip PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip
 
-    cd 'PhotoGIMP by Diolinux v2020 for Flatpak/'
-    
-    dirs_to_move=(
-        .var
-        .icons
-        .local/share/applications
-    )
-    for path in ${dirs_to_move[@]} ; do
-        if [ ! -d ~/$path ] ; then
-            mkdir -p ~/$path
-            mv $path/* ~/$path
-        else
-            mv $path/* ~/$path
-        fi
-    done
-
-    # This is because rofi doesn't show the PhotoGIMP icon in ~/.icons,
-    # but can show the icon that Papirus have.
-    if ( find /usr/share/icons -name "photogimp*" >/dev/null ) ; then
-        sed -i "s/.png//g" ~/.local/share/applications/org.gimp.GIMP.desktop
+  cd 'PhotoGIMP by Diolinux v2020 for Flatpak/'
+  
+  dirs_to_move=(
+    .var
+    .icons
+    .local/share/applications
+  )
+  for path in ${dirs_to_move[@]}; do
+    if [[ ! -d ~/$path ]]; then
+      mkdir -p ~/$path
+      mv $path/* ~/$path
+    else
+      mv $path/* ~/$path
     fi
+  done
 
-    cd .. && rm -rf 'PhotoGIMP by Diolinux v2020 for Flatpak'
-    echo -e "\033[1;31mThe GIMP icon will change to PhotoGIMP after you restart.\033[0m"
+  # This is because rofi doesn't show the PhotoGIMP icon in ~/.icons,
+  # but can show the icon that Papirus have.
+  if ( find /usr/share/icons -name "photogimp*" >/dev/null ); then
+    sed -i "s/.png//g" ~/.local/share/applications/org.gimp.GIMP.desktop
+  fi
+
+  cd .. && rm -rf 'PhotoGIMP by Diolinux v2020 for Flatpak'
+  echo -e "\033[1;31mThe GIMP icon will change to PhotoGIMP after you restart.\033[0m"
 fi
 
 ## Emojis
--- post-installation.sh
+++ post-installation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ "$(whoami)" == "root" ]; then
-    echo "Script must be run as a normal user"
-    exit
+  echo "Script must be run as a normal user"
+  exit
 fi
 
 # Directory where the script is
@@ -41,22 +41,21 @@ EOF'
 
 ## Generating a SSH Key for GitHub
 if ( ! grep -q ".pub" <<< $(ls ~/.ssh 2>/dev/null)); then
-    read -p "Enter your github email: " git_email
-    read -p "Enter your github user name: " git_name
+  read -p "Enter your github email: " git_email
+  read -p "Enter your github user name: " git_name
 
-    ssh-keygen -t ed25519 -C "$git_email"
+  ssh-keygen -t ed25519 -C "$git_email"
 
-    eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/id_ed25519
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_ed25519
 
-    echo -e "\033[1;29mThat's the key you have to copy:
-    \033[0;32m$(cat ~/.ssh/id_ed25519.pub)\033[0m"
+  echo -e "\033[1;29mThat's the key you have to copy:
+  \033[0;32m$(cat ~/.ssh/id_ed25519.pub)\033[0m"
 
-    git config --global user.name "$git_name"
-    git config --global user.email $git_email
-    git config --global core.editor nvim
-    git config --global init.defaultBranch master
-
+  git config --global user.name "$git_name"
+  git config --global user.email $git_email
+  git config --global core.editor nvim
+  git config --global init.defaultBranch master
 fi
 
 echo -e"\n\nEverything is done!"
